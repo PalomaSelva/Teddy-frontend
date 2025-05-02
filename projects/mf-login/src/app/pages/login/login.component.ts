1eb602b5896a9bbd0d0a63c96ad7168066cd1f93
PalomaSelva <palomaselva1@email.com>
2025-05-02 03:35:04 -0300

    feat: integrate reactive forms in login component and update routing

--- projects/mf-login/src/app/pages/login/login.component.ts
+++ projects/mf-login/src/app/pages/login/login.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+    });
+  }
 }
