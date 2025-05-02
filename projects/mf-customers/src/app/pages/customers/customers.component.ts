807f91d59ce832c2f4e11cb8ed3c2520d3e9a897
PalomaSelva <palomaselva1@email.com>
2025-05-02 05:42:54 -0300

    feat: add ng-bootstrap pagination to customers component

--- projects/mf-customers/src/app/pages/customers/customers.component.ts
+++ projects/mf-customers/src/app/pages/customers/customers.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../shared/components/card/card.component';
+import '@angular/localize/init';
 
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-customers',
-  imports: [CardComponent],
+  imports: [CardComponent, NgbPaginationModule],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.scss',
 })
-export class CustomersComponent {}
+export class CustomersComponent {
+  page = 1;
+}
