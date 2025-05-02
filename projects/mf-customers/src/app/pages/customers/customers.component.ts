ac708ba2395150abd14efdcad9e1e8e021280d17
PalomaSelva <palomaselva1@email.com>
2025-05-02 12:34:23 -0300

    feat: implement dynamic customer listing in CustomersComponent with pagination support

--- projects/mf-customers/src/app/pages/customers/customers.component.ts
+++ projects/mf-customers/src/app/pages/customers/customers.component.ts
@@ -3,12 +3,40 @@ import { CardComponent } from '../../shared/components/card/card.component';
 import '@angular/localize/init';
 
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { CustomersService } from '../../shared/services/customers/customers.service';
+import { CustomerResponse } from '../../shared/interfaces/customer.interface';
+
 @Component({
   selector: 'app-customers',
+  standalone: true,
   imports: [CardComponent, NgbPaginationModule],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.scss',
 })
 export class CustomersComponent {
   page = 1;
+  customers: CustomerResponse[] = [];
+  totalPages = 0;
+  totalItems = 0;
+  itemsPerPage = 10;
+
+  constructor(private _customersService: CustomersService) {}
+
+  ngOnInit(): void {
+    this.getCustomers();
+  }
+
+  getCustomers() {
+    this._customersService
+      .getCustomers(this.page, this.itemsPerPage)
+      .subscribe({
+        next: (res: any) => {
+          this.customers = res.clients;
+          this.totalPages = res.totalPages;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
 }
--- projects/mf-customers/src/app/shared/services/customers/customers.service.ts
+++ projects/mf-customers/src/app/shared/services/customers/customers.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomersService {
   apiUrl: string = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
-  getCustomers() {
-    return this.http.get(`${environment.apiUrl}/customers`);
+  getCustomers(page: number, limit: number) {
+    const params = new HttpParams().set('page', page).set('limit', limit);
+    return this.http.get(`${environment.apiUrl}/users`, { params });
   }
 }
