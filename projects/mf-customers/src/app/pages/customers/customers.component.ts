655e0f6871728015fbb1679d69dfc58df91246c5
PalomaSelva <palomaselva1@email.com>
2025-05-02 05:22:23 -0300

    feat: update customers component layout with new card elements and responsive design

--- projects/mf-customers/src/app/pages/customers/customers.component.ts
+++ projects/mf-customers/src/app/pages/customers/customers.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-customers',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './customers.component.html',
-  styleUrl: './customers.component.scss'
+  styleUrl: './customers.component.scss',
 })
-export class CustomersComponent {
-
-}
+export class CustomersComponent {}
