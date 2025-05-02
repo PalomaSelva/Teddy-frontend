ba270ce409ab409b3c39c0b04f8b6bed8b0d5e41
PalomaSelva <palomaselva1@email.com>
2025-05-02 11:54:01 -0300

    feat: update menu component to use button for logout and apply title case to user greeting

--- projects/host/src/shared/components/menu/menu.component.ts
+++ projects/host/src/shared/components/menu/menu.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from '../sidebar/sidebar.component';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common';
+
+import { CommonModule, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
