bceeaeb88d68eebb0f571ca681a9f7031637c4a7
PalomaSelva <palomaselva1@email.com>
2025-05-02 22:52:32 -0300

    feat: create InputTextComponent for reusable input fields with validation and masking support

---
+++ projects/mf-customers/src/app/shared/components/input-text/input-text.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective } from 'ngx-mask';
+import { ErrorMessageService } from '../../services/error-message/error-message.service';
+
+@Component({
+  selector: 'app-input-text',
+  imports: [ReactiveFormsModule, CommonModule, NgxMaskDirective],
+  templateUrl: './input-text.component.html',
+  styleUrl: './input-text.component.scss',
+})
+export class InputTextComponent {
+  @Input() label: string = '';
+  @Input() control: FormControl = new FormControl();
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() mask: string = '';
+  @Input() currency: boolean = false;
+  @Input() maxlength?: string;
+  @Input() minlength?: string;
+  @Input() readOnly?: boolean;
+  @Output() input = new EventEmitter<any>();
+  @Output() change = new EventEmitter<any>();
+
+  maskOptions: any = {};
+
+  constructor(private errorMessageService: ErrorMessageService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    if (changes['currency']) {
+      this.mask = 'separator.2';
+      this.maskOptions = {
+        separator: '.',
+        thousandSeparator: ',',
+        prefix: 'R$ ',
+        allowNegative: false,
+      };
+    }
+  }
+
+  get errorMessage(): string | null {
+    const message = this.errorMessageService.getErrorMessage(this.control);
+
+    return message;
+  }
+}
