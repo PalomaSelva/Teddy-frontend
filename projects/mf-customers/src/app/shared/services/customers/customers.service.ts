b0146c710c43fa6dd584cb29ae09d6dec663633f
PalomaSelva <palomaselva1@email.com>
2025-05-02 22:00:36 -0300

    feat: implement CustomersService with create, update, and delete methods

--- projects/mf-customers/src/app/shared/services/customers/customers.service.ts
+++ projects/mf-customers/src/app/shared/services/customers/customers.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { CustomerRequest } from '../../interfaces/customer.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +14,17 @@ export class CustomersService {
   getCustomers(page: number, limit: number) {
     const params = new HttpParams().set('page', page).set('limit', limit);
     return this.http.get(`${environment.apiUrl}/users`, { params });
+  }
+
+  createCustomer(customer: CustomerRequest) {
+    return this.http.post(`${environment.apiUrl}/users`, customer);
+  }
+
+  updateCustomer(customer: CustomerRequest, id: number) {
+    return this.http.patch(`${environment.apiUrl}/users/${id}`, customer);
+  }
+
+  deleteCustomer(id: number) {
+    return this.http.delete(`${environment.apiUrl}/users/${id}`);
   }
 }
