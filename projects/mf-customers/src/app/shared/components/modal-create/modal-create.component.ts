fae1f4cf42ce15f1be29806b84e849938f35994a
PalomaSelva <palomaselva1@email.com>
2025-05-02 23:03:57 -0300

    feat: refactor modal-create component to use reusable input fields and improve form handling with validation and close functionality

--- projects/mf-customers/src/app/shared/components/modal-create/modal-create.component.ts
+++ projects/mf-customers/src/app/shared/components/modal-create/modal-create.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { CustomerRequest } from '../../interfaces/customer.interface';
+import { CustomersService } from '../../services/customers/customers.service';
+import { InputTextComponent } from '../input-text/input-text.component';
 
 @Component({
   selector: 'app-modal-create',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, InputTextComponent],
   templateUrl: './modal-create.component.html',
   styleUrl: './modal-create.component.scss',
 })
 export class ModalCreateComponent {
+  @ViewChild('closeModal') closeModalElement!: ElementRef<HTMLButtonElement>;
+  @Output() saveCustomer: EventEmitter<CustomerRequest> =
+    new EventEmitter<CustomerRequest>();
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    protected _customersService: CustomersService
+  ) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -23,5 +39,18 @@ export class ModalCreateComponent {
       salary: ['', Validators.required],
       companyValuation: ['', Validators.required],
     });
+  }
+
+  createCustomer() {
+    const customer: CustomerRequest = this.form.value;
+    this.saveCustomer.emit(customer);
+  }
+
+  closeModal() {
+    this.closeModalElement.nativeElement.click();
+  }
+
+  getControl(control: string) {
+    return this.form.get(control) as FormControl;
   }
 }
