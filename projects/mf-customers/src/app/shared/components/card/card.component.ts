521d438edf47b2c535d7f83bf28c12201e837000
PalomaSelva <palomaselva1@email.com>
2025-05-02 23:38:59 -0300

    feat: update card component to use event emitters for edit and delete actions, integrate modal-delete component for improved customer management

--- projects/mf-customers/src/app/shared/components/card/card.component.ts
+++ projects/mf-customers/src/app/shared/components/card/card.component.ts
@@ -2,37 +2,25 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerResponse } from '../../interfaces/customer.interface';
 import { ModalCreateComponent } from '../modal-create/modal-create.component';
+import { ModalDeleteComponent } from '../modal-delete/modal-delete.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ModalDeleteComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  @Input() item: CustomerResponse = {
-    id: 0,
-    name: '',
-    salary: 0,
-    companyValuation: 0,
-    createdAt: '',
-    updatedAt: '',
-  };
-  @Output('selectCustomer') selectCustomerEmitter = new EventEmitter<void>();
-  @Output('modalEdit') modalEditEmitter = new EventEmitter<CustomerResponse>();
-  @Output('modalDelete') modalDeleteEmitter =
-    new EventEmitter<CustomerResponse>();
+  @Input() item!: CustomerResponse;
+  @Output() onDelete = new EventEmitter<CustomerResponse>();
+  @Output() onEdit = new EventEmitter<CustomerResponse>();
 
-  selectCustomer() {
-    // this.selectCustomerEmitter.emit(this.item);
+  openDeleteModal(customer: CustomerResponse) {
+    this.onDelete.emit(customer);
   }
 
-  openModalEdit(item: CustomerResponse) {
-    this.modalEditEmitter.emit(item);
-  }
-
-  openModalDelete(item: CustomerResponse) {
-    this.modalDeleteEmitter.emit(item);
+  editCustomer(customer: CustomerResponse) {
+    this.onEdit.emit(customer);
   }
 }
