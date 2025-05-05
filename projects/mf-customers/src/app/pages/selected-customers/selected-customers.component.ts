2f18ae0fa7b3bfd081d33ba968980de407d24dd5
PalomaSelva <palomaselva1@email.com>
2025-05-05 13:39:00 -0300

    feat: implement ngOnInit lifecycle hook in SelectedCustomersComponent for initializing customer data

--- projects/mf-customers/src/app/pages/selected-customers/selected-customers.component.ts
+++ projects/mf-customers/src/app/pages/selected-customers/selected-customers.component.ts
@@ -14,7 +14,11 @@ import { CommonModule } from '@angular/common';
 export class SelectedCustomersComponent {
   customers: CustomerResponse[] = [];
 
-  constructor(private selectedCustomersService: SelectedCustomersService) {
+  constructor(private selectedCustomersService: SelectedCustomersService) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
     this.selectedCustomersService
       .getSelectedCustomers()
       .subscribe((customers) => {
