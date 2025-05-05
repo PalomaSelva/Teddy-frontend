e6a72712a4b87c952d98651b6e54e8048ee4c8cc
PalomaSelva <palomaselva1@email.com>
2025-05-05 04:21:52 -0300

    refactor: add console logs to bootstrap and main files for better debugging

--- projects/host/src/main.ts
+++ projects/host/src/main.ts
@@ -1,6 +1,6 @@
 import { initFederation } from '@angular-architects/native-federation';
-
+console.log('Main.ts carregado!');
 initFederation('federation.manifest.json')
-  .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err))
+  .then((_) => import('./bootstrap'))
+  .catch((err) => console.error(err));
