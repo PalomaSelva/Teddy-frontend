a2b3b543d72cb9f88588a3c19e97242fd5c5b3a3
PalomaSelva <palomaselva1@email.com>
2025-05-05 01:20:31 -0300

    test: update CustomersService tests with HttpClient provider

--- projects/mf-customers/src/app/shared/services/customers/customers.service.spec.ts
+++ projects/mf-customers/src/app/shared/services/customers/customers.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CustomersService } from './customers.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CustomersService', () => {
   let service: CustomersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [CustomersService, provideHttpClient()],
+    });
     service = TestBed.inject(CustomersService);
   });
 
